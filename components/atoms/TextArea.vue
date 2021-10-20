17915281839d929a92e481420170ad258f048ab7
Florian Wächter <system@gammagate.de>
2021-10-20 21:00:15 +0200

    Editor buttons for textarea

--- components/atoms/TextArea.vue
+++ components/atoms/TextArea.vue
@@ -1,22 +1,101 @@
 <template>
-  <v-container v-if="type === 'accordion'">
-    <v-textarea
-      clearable
-      label="Description above the accordion"
-      @change="addText"
-    ></v-textarea>
-  </v-container>
+  <v-container>
+    <v-toolbar class="d-flex justify-center" elevation="2" dense rounded>
+      <template>
+        <v-btn-toggle
+          v-model="toggle_multiple"
+          color="primary"
+          dense
+          multiple
+          rounded
+        >
+          <v-btn :value="1" @click="bold = !bold">
+            <v-icon>mdi-format-bold</v-icon>
+          </v-btn>
 
-  <v-container v-else-if="type === 'slider'">
-    <v-textarea
-      clearable
-      label="Description above the slider"
-      @change="addText"
-    ></v-textarea>
-  </v-container>
+          <v-btn :value="2" @click="italic = !italic">
+            <v-icon>mdi-format-italic</v-icon>
+          </v-btn>
 
-  <v-container v-else-if="type === 'text'">
-    <v-textarea clearable label="Description" @change="addText"></v-textarea>
+          <v-btn :value="3" @click="underline = !underline">
+            <v-icon>mdi-format-underline</v-icon>
+          </v-btn>
+
+          <v-btn :value="4">
+            <v-icon>mdi-format-color-fill</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+
+        <div class="mx-4"></div>
+
+        <v-btn-toggle
+          v-model="toggle_exclusive"
+          color="primary"
+          dense
+          mandatory
+          rounded
+        >
+          <v-btn :value="1">
+            <v-icon>mdi-format-align-left</v-icon>
+          </v-btn>
+
+          <v-btn :value="2">
+            <v-icon>mdi-format-align-center</v-icon>
+          </v-btn>
+
+          <v-btn :value="3">
+            <v-icon>mdi-format-align-right</v-icon>
+          </v-btn>
+
+          <v-btn :value="4">
+            <v-icon>mdi-format-align-justify</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </template>
+    </v-toolbar>
+
+    <v-container v-if="type === 'accordion'">
+      <v-textarea
+        :class="{
+          txtbold: bold,
+          txtitalic: italic,
+          txtunderline: underline,
+        }"
+        clearable
+        label="Beschreibung"
+        color="var(--lightgreen-contrast)"
+        @change="addText"
+      >
+      </v-textarea>
+    </v-container>
+
+    <v-container v-else-if="type === 'slider'">
+      <v-textarea
+        :class="{
+          txtbold: bold,
+          txtitalic: italic,
+          txtunderline: underline,
+        }"
+        clearable
+        label="Beschreibung"
+        color="var(--lightgreen-contrast)"
+        @change="addText"
+      ></v-textarea>
+    </v-container>
+
+    <v-container v-else-if="type === 'text'">
+      <v-textarea
+        :class="{
+          txtbold: bold,
+          txtitalic: italic,
+          txtunderline: underline,
+        }"
+        clearable
+        label="Inhalt"
+        color="var(--lightgreen-contrast)"
+        @change="addText"
+      ></v-textarea>
+    </v-container>
   </v-container>
 </template>
 
@@ -27,6 +106,11 @@ import { Vue, Component, Emit, Prop } from 'nuxt-property-decorator';
 export default class TextArea extends Vue {
   @Prop({ default: 'accordion' })
   type!: string;
+  bold = false;
+  italic = false;
+  underline = false;
+  toggle_exclusive = 1;
+  toggle_multiple = [];
 
   @Emit('textarea')
   addText(text: any) {
@@ -34,3 +118,17 @@ export default class TextArea extends Vue {
   }
 }
 </script>
+
+<style scoped>
+.txtbold {
+  font-weight: bold;
+}
+
+.txtitalic {
+  font-style: italic;
+}
+
+.txtunderline {
+  text-decoration: underline;
+}
+</style>

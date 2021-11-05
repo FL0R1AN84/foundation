80d4dce550cf60f8ddcbb6f69d52d51dac2bd606
Florian Wächter <system@gammagate.de>
2021-11-05 19:01:26 +0100

    Remove second template tag

--- components/atoms/TextArea.vue
+++ components/atoms/TextArea.vue
@@ -1,57 +1,55 @@
 <template>
   <v-container>
     <v-toolbar class="d-flex justify-center" elevation="2" dense rounded>
-      <template>
-        <v-btn-toggle
-          v-model="toggle_multiple"
-          color="primary"
-          dense
-          multiple
-          rounded
-        >
-          <v-btn :value="1" @click="bold = !bold">
-            <v-icon>mdi-format-bold</v-icon>
-          </v-btn>
+      <v-btn-toggle
+        v-model="toggle_multiple"
+        color="primary"
+        dense
+        multiple
+        rounded
+      >
+        <v-btn :value="1" @click="bold = !bold">
+          <v-icon>mdi-format-bold</v-icon>
+        </v-btn>
 
-          <v-btn :value="2" @click="italic = !italic">
-            <v-icon>mdi-format-italic</v-icon>
-          </v-btn>
+        <v-btn :value="2" @click="italic = !italic">
+          <v-icon>mdi-format-italic</v-icon>
+        </v-btn>
 
-          <v-btn :value="3" @click="underline = !underline">
-            <v-icon>mdi-format-underline</v-icon>
-          </v-btn>
+        <v-btn :value="3" @click="underline = !underline">
+          <v-icon>mdi-format-underline</v-icon>
+        </v-btn>
 
-          <v-btn :value="4">
-            <v-icon>mdi-format-color-fill</v-icon>
-          </v-btn>
-        </v-btn-toggle>
+        <v-btn :value="4">
+          <v-icon>mdi-format-color-fill</v-icon>
+        </v-btn>
+      </v-btn-toggle>
 
-        <div class="mx-4"></div>
+      <div class="mx-4"></div>
 
-        <v-btn-toggle
-          v-model="toggle_exclusive"
-          color="primary"
-          dense
-          mandatory
-          rounded
-        >
-          <v-btn :value="1">
-            <v-icon>mdi-format-align-left</v-icon>
-          </v-btn>
+      <v-btn-toggle
+        v-model="toggle_exclusive"
+        color="primary"
+        dense
+        mandatory
+        rounded
+      >
+        <v-btn :value="1">
+          <v-icon>mdi-format-align-left</v-icon>
+        </v-btn>
 
-          <v-btn :value="2">
-            <v-icon>mdi-format-align-center</v-icon>
-          </v-btn>
+        <v-btn :value="2">
+          <v-icon>mdi-format-align-center</v-icon>
+        </v-btn>
 
-          <v-btn :value="3">
-            <v-icon>mdi-format-align-right</v-icon>
-          </v-btn>
+        <v-btn :value="3">
+          <v-icon>mdi-format-align-right</v-icon>
+        </v-btn>
 
-          <v-btn :value="4">
-            <v-icon>mdi-format-align-justify</v-icon>
-          </v-btn>
-        </v-btn-toggle>
-      </template>
+        <v-btn :value="4">
+          <v-icon>mdi-format-align-justify</v-icon>
+        </v-btn>
+      </v-btn-toggle>
     </v-toolbar>
 
     <v-container v-if="type === 'accordion'">
@@ -106,6 +104,7 @@ import { Vue, Component, Emit, Prop } from 'nuxt-property-decorator';
 export default class TextArea extends Vue {
   @Prop({ default: 'accordion' })
   type!: string;
+
   bold = false;
   italic = false;
   underline = false;
